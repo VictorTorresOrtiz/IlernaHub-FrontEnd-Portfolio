5281e1f7693ee0268fa5f0d052eac98e96bbcdfe
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2022-11-16 18:31:55 +0100

    sw Delete cache modification

--- sw.js
+++ sw.js
@@ -44,3 +44,22 @@ self.addEventListener("activate", (event) => {
         })
     );
 });
+
+
+
+//Delete old Cache
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.filter(function(cacheName) {
+            // Return true if you want to remove this cache,
+            // but remember that caches are shared across
+            // the whole origin
+          }).map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+    );
+  });
