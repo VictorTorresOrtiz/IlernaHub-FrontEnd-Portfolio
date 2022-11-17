398cfdea4f9df1fd6d0be055e39520e9ab8b835a
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2022-11-17 10:43:20 +0100

    Adding Assets

---
+++ IlernaHub-Frontend(CON PHP)/includes/index.php
@@ -0,0 +1,230 @@
+<?php
+  session_start();
+  include('bdd/config.php');
+  if(isset($_POST['login']))
+  {
+    $status='1';
+    $email=$_POST['username'];
+    $password=md5($_POST['password']);
+    $sql ="SELECT email,password FROM users WHERE email=:email and password=:password and status=(:status)";
+    $query= $dbh -> prepare($sql);
+    $query-> bindParam(':email', $email, PDO::PARAM_STR);
+    $query-> bindParam(':password', $password, PDO::PARAM_STR);
+    $query-> bindParam(':status', $status, PDO::PARAM_STR);
+    $query-> execute();
+    $results=$query->fetchAll(PDO::FETCH_OBJ);
+      if($query->rowCount() > 0)
+      {
+      $_SESSION['alogin']=$_POST['username'];
+      echo "<script type='text/javascript'> document.location = 'profile.php'; </script>";
+      } else{
+        
+        echo "<script>alert('Invalid Details Or Account Not Confirmed');</script>";
+
+      }
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>IlernaHub || Online Launcher</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+
+    <!--PWA ASSETS-->
+    <link rel="manifest" href="manifest.json">
+    <!-- IOS SUPPORT -->
+    <link rel="apple-touch-icon" href="images/icons/icon-72x72.png" />
+    <link rel="apple-touch-icon" href="images/icons/icon-96x96.png" />
+    <link rel="apple-touch-icon" href="images/icons/icon-128x128.png" />
+    <link rel="apple-touch-icon" href="images/icons/icon-144x144.png" />
+    <link rel="apple-touch-icon" href="images/icons/icon-152x152.png" />
+    <link rel="apple-touch-icon" href="images/icons/icon-192x192.png" />
+    <link rel="apple-touch-icon" href="images/icons/icon-384x384.png" />
+    <link rel="apple-touch-icon" href="images/icons/icon-512x512.png" />
+    <meta name="apple-mobile-web-app-status-bar" content="#db4938" />
+    <meta name="theme-color" content="#db4938" />
+
+
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        <img src="assets/images/logo.png" alt="">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Search End ***** -->
+                    <div class="search-input">
+                      <form id="search" action="#">
+                        <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword" onkeypress="handle" />
+                        <i class="fa fa-search"></i>
+                      </form>
+                    </div>
+                    <!-- ***** Search End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li><a href="index.html" class="active">Home</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="profile.php">Welcome Back!<img src="assets/images/profile-header.jpg" alt=""></a></li>
+                    </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="page-content">
+
+          <!-- ***** Banner Start ***** -->
+          <div class="main-banner">
+            <div class="row">
+              <div class="col-lg-7">
+                <div class="header-text">
+                  <h6>Welcome to IlernaHub</h6>
+                  <h4><em>Browse</em> our Apps online</h4>
+                  <div class="main-button">
+                    <a href="#">Browse Now</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- ***** Banner End ***** -->
+
+          <!-- ***** LAUNCHED APPS ***** -->
+          <div class="most-popular">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="heading-section">
+                  <h4><em>Launched</em> Apps</h4>
+                </div>
+                <div class="row">
+                  <div class="col-lg-3 col-sm-6">
+                    <div class="item">
+                      <img src="assets/images/apps_assets/chatReal.png" alt="">
+                      <a href="detailsChat.html">
+                      <h4>Ilerna Chat<br><span>Live Chat App<br><br><br></span></h4>
+                      </a>
+                      <ul>
+                        <li><i class="fab fa-github"></i></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                    <div class="item">
+                      <img src="assets/images/apps_assets/cloud.png" alt="">
+                      <h4>Ilerna Cloud<br><span>Cloud System<br><br><br></span></h4>
+                      <ul>
+                        <li><i class="fab fa-github"></i></li>
+                      </ul>
+                    </div>
+                  </div>          
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- ***** LAUNCHED APPS ***** -->
+
+            <!-- ***** Most Popular Start ***** -->
+            <div class="most-popular">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="heading-section">
+                    <h4><em>Coming Soon</em> Apps</h4>
+                  </div>
+                  <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+                      <div class="item">
+                        <img src="assets/images/apps_assets/alc.png" alt="">
+                        <h4>Alcalá a la Carta<br><span>Listing App</span></h4>
+                        <ul>
+                          <li><i class="fab fa-github"></i></li>
+                        </ul>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <!-- ***** Most Popular End ***** -->
+
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>Copyright © 2022 <a href="#">IlernaDaw</a> || All rights reserved. 
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/tabs.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+   <!--JS PWA-->
+   <script src="pwa.js"></script>
+   <script src="serviceWorker.js"></script>
+
+
+  </body>
+
+</html>
