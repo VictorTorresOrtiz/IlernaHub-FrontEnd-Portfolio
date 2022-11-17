1751f8d57575777488d48ddd253ac4e7de4b3fba
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2022-11-17 09:19:05 +0100

    Adding Features

--- IlernaHub-Frontend(CON PHP)/UserManagement/login.php
+++ IlernaHub-Frontend(CON PHP)/UserManagement/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('includes/config.php');
+include('./profile.php');
 if(isset($_POST['login']))
 {
 $status='1';
@@ -16,7 +16,7 @@ $results=$query->fetchAll(PDO::FETCH_OBJ);
 if($query->rowCount() > 0)
 {
 $_SESSION['alogin']=$_POST['username'];
-echo "<script type='text/javascript'> document.location = '../includes/index.php'; </script>";
+echo "<script type='text/javascript'> document.location = '../../includes/index.php'; </script>";
 } else{
   
   echo "<script>alert('Invalid Details Or Account Not Confirmed');</script>";
