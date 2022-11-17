177a06cf0ed4b4e63d949b8db6e1f492a417f672
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2022-11-17 09:18:39 +0100

    Adding Features

--- IlernaHub-Frontend(CON PHP)/UserManagement/includes/leftbar.php
+++ IlernaHub-Frontend(CON PHP)/UserManagement/includes/leftbar.php
@@ -4,14 +4,11 @@
 			<li class="ts-label">Main</li>
 			<li><a href="profile.php"><i class="fa fa-user"></i> &nbsp;Profile</a>
 			</li>
-			<li><a href="feedback.php"><i class="fa fa-envelope"></i> &nbsp;Feedback</a>
+			<li><a href="#"><i class="fa fa-envelope"></i> &nbsp;Mail (Coming Soon)<sup style="color:red">*</sup></a>
 			</li>
 			<li><a href="notification.php"><i class="fa fa-bell"></i> &nbsp;Notification<sup style="color:red">*</sup></a>
 			</li>
-			<li><a href="messages.php"><i class="fa fa-envelope"></i> &nbsp;Messages</a>
-			</li>
 			</ul>
-			<p class="text-center" style="color:#ffffff; margin-top: 100px;">© Ajay</p>
 		</nav>
 
 		
